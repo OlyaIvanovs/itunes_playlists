ba9f7a6514878b6f32217122932138a9ca05e921
olya <olya@ivanovs.info>
2021-05-30 20:44:19 +1200

    Tower is complete.

--- tower_hanoi/tower.py
+++ tower_hanoi/tower.py
@@ -19,8 +19,19 @@ SOLVED_TOWER = list(range(TOTAL_DISKS, 0, -1))
 
 
 def display_towers(towers):
-    """"""
-    pass
+    """Display the three towers with their disks."""
+    # Display the three towers with their disks.
+    for level in range(TOTAL_DISKS, -1, -1):
+        for tower in (towers["A"], towers["B"], towers["C"]):
+            if level >= len(tower):
+                display_disk(0)  # Display the bare pole with no disk
+            else:
+                display_disk(tower[level])
+        print()
+
+    # Display the tower labels A, B, C
+    empty_space = " " * (TOTAL_DISKS)
+    print("{0} A{0}{0} B{0}{0} C\n".format(empty_space))
 
 
 def display_disk(width):
@@ -31,12 +42,39 @@ def display_disk(width):
     else:
         center = "@"*width + f"_{width}" + "@"*width
     disk = f"{empty_space}{center}{empty_space}"
-    print(disk)
+    print(disk, end="")
 
 
-def get_player_move():
-    """"""
-    pass
+def get_player_move(towers):
+    """Asks the player for a move. Returns (fromTower, toTower)."""
+    while True:
+        print('Enter the letters of "from" and "to" towers or QUIT')
+        print("(e.g., AB to moves a disk from tower A to tower B.)")
+        print()
+        response = input("> ").upper().strip()
+
+        if response == 'QUIT':
+            print("Thanks for playing!")
+            sys.exit()
+
+        if response not in ("AB", "AC", "BA", "BC", "CA", "CB"):
+            print("Enter one of AB, AC, BA, BC, CA, CB")
+            continue
+
+        from_tower, to_tower = response[0], response[1]
+        print(from_tower, to_tower)
+
+        if len(towers[from_tower]) == 0:
+            # The "from" tower cannot be an empty tower:
+            print("You selected a tower with no disks.")
+            continue
+        elif len(towers[to_tower]) == 0:
+            return from_tower, to_tower
+        elif towers[from_tower][-1] > towers[to_tower][-1]:
+            print("Can't put larger disks on top of smaller ones.")
+            continue
+        else:
+            return from_tower, to_tower
 
 
 def main():
@@ -64,6 +102,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    for i in range(6):
-        display_disk(i)
+    main()
